a49e32168fe03cd19bd252d43522dd7ea960520f
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-12-09 14:58:33 -0300

    Adicionando tela Login, funções e hooks.

---
+++ src/screens/LoginPage/styled.js
@@ -0,0 +1,37 @@
+import Icon from "@mdi/react";
+import styled from "styled-components";
+
+export const ScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+`
+
+export const LoginIcon = styled(Icon)`
+  display: block;
+  color: rgba(0, 0, 0, 0.87);
+  /* color: #3f51b5; */
+  width: 40vw;
+  max-width: 160px;
+  margin: 20px;
+`
+
+export const LoginFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  max-width: 480px;
+  margin-bottom: 16px;
+`
+
+export const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+`
+
+export const SignUpButtonContainer = styled.div`
+  width: 80vw;
+  max-width: 480px;
+`
